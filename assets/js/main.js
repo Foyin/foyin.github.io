53af2936d0e48b0960f43291091aa9f904fb024e
Foyin Ogbara <foyinogb@live.co.uk>
2019-09-02 17:24:17 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -232,7 +232,7 @@ emailjs.sendForm(service_id,template_id,myform[0]).then(
 		alert("Sent!");
 		$("#contactForm .row .col-12").fadeOut();
 		$("#contactForm .row .col-12").remove();
-		$("#contactForm .row").prepend('<h2 style="width:100%; padding:1%; text-align:center;"><b>Thank You!</b><h2>');
+		$("#contactForm .row").prepend('<h2 style="width:100%; padding:25%; text-align:center;"><b>Thank You!</b><h2>');
 	}, 
 	function(err) {
 		alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
