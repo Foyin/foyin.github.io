ffd415583b77275b5dc7a20d6ba5621e2eec0e88
Foyin Ogbara <foyinogb@live.co.uk>
2022-04-26 10:10:50 -0400

    Update main.js

--- main.js
+++ main.js
@@ -243,7 +243,6 @@ function init(){
 
   if (Math.random() >= 0.6){
     scene.add(mars);
-    scene.add(torus);
   }
   else if(Math.random() > 0.6 && Math.random() <= 0.8){
     scene.add(saturn);
